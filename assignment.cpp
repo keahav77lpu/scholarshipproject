bef85236cf71ed6c845d425ba4d9f765dfb01d64
keahav77lpu <46514502+keahav77lpu@users.noreply.github.com>
2019-11-28 16:09:18 +0530

    Add files via upload
    assignment.cpp

---
+++ assignment.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string name,phone,email;
+	cin>>name>>phone>>email;
+	cout<<name<<endl;
+	cout<<phone<<endl;
+	cout<<email<<endl;
+}
